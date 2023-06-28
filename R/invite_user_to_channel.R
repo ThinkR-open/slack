1e9e3225816ace0cd13650e7ca4e18fa513369e2
Vincent Guyader <vincent@thinkr.fr>
2023-06-28 21:32:12 +0000

    remote current user from invite_users_to_channel

--- R/invite_user_to_channel.R
+++ R/invite_user_to_channel.R
@@ -4,13 +4,14 @@
 #' @param channel channel name
 #' @param create_channel boolean create chanel if needed
 #' @param token slack api token
+#' @param current_user current user id
 #'
 #' @importFrom purrr map
 #' @importFrom magrittr %>%
 #' @export
 #'
 invite_users_to_channel <- function(channel, users, token=Sys.getenv("SLACK_API_TOKEN"), create_channel = TRUE,
-                                    all_channel = slackr::slackr_channels()){
+                                    all_channel = slackr::slackr_channels(),current_user = slackr::auth_test()$user_id){
 
   if (create_channel &
       length(get_channel_id(tolower(channel),all_channel = all_channel)) == 0) {
@@ -18,18 +19,12 @@ invite_users_to_channel <- function(channel, users, token=Sys.getenv("SLACK_API_
     # Sys.sleep(1)
   }
 
-  for (u in users){
-
   httr::POST(url="https://slack.com/api/conversations.invite",
-               body=list( token=token,
-                          channel= get_channel_id(name = tolower(channel),all_channel = all_channel),
-                          users=get_user_id(u)))
-    Sys.sleep(1)
-
-  }
-
-
-    invisible(channel)
+             body=list( token=token,
+                        channel= get_channel_id(name = tolower(channel),all_channel = all_channel),
+                        users=paste(     setdiff(  get_user_id(users),current_user )  ,collapse=",")))
+  # Sys.sleep(1)
+  invisible(channel)
 }
 
 
